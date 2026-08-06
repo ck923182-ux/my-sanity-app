eda0955d40fca265901f04fd2efd67ba6a7a5459
Chandan Kumar <ck923182@gmail.com>
2026-08-07 00:06:55 +0530

    Update Hero Sanity filed with One variation

--- blog-cms/schemaTypes/objects/hero.ts
+++ blog-cms/schemaTypes/objects/hero.ts
@@ -1,39 +1,66 @@
-import { defineField, defineType } from "sanity";
+import {defineField, defineType} from 'sanity'
 
 export const heroType = defineType({
-    name: "hero",
-    title: "Hero Section",
-    type: "object",
-    fields: [
-        defineField({
-            name: "heading",
-            title: "Heading",
-            type: "string",
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: "subheading",
-            title: "Sub Heading",
-            type: "string",
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: "content",
-            title: "Content",
-            type: "text",
-            rows: 3
-        }),
-        defineField({
-            name: "heroButton",
-            title: "Button",
-            type: "array",
-            of: [
-                {
-                    type: "button",
-                },
-
-            ],
-        }),
-    ],
-
-});
+  name: 'hero',
+  title: 'Hero Section',
+  type: 'object',
+  fields: [
+    defineField({
+      name: 'heading',
+      title: 'Heading',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'subheading',
+      title: 'Sub Heading',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+      rows: 3,
+    }),
+    defineField({
+      name: 'heroButton',
+      title: 'Button',
+      type: 'array',
+      of: [
+        {
+          type: 'button',
+        },
+      ],
+    }),
+    defineField({
+      name: 'Highlights',
+      title: 'Highlights',
+      type: 'string',
+    }),
+    defineField({
+      name: 'Highlightscards',
+      title: 'Highlights Cards',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          name: 'highlightCard',
+          title: 'Highlight Card',
+          fields: [
+            defineField({
+              name: 'Highlightsnumber',
+              title: 'Highlights Value',
+              type: 'number',
+            }),
+            defineField({
+              name: 'text',
+              title: 'Text',
+              type: 'string',
+            }),
+          ],
+        },
+      ],
+    }),
+  ],
+})
