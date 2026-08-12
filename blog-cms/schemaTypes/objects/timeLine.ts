b2bff9b46be2188ac51da6f7a38e1e2ec9556929
Chandan Kumar <ck923182@gmail.com>
2026-08-12 22:47:00 +0530

    All Page Bulder Component created

---
+++ blog-cms/schemaTypes/objects/timeLine.ts
@@ -0,0 +1,29 @@
+import { defineField,defineType } from "sanity";
+
+export const timeLine = defineType({
+    name:"timeline",
+    title:"Time Line",
+    type:"object",
+    fields:[
+        defineField({
+           name:"title",
+           title:"Title",
+           type:"string",
+        }),
+        defineField({
+            name:"",
+            title:"",
+            type: 'icon.manager',
+        }),
+        defineField({
+            name:"year",
+            title:"Year",
+            type:"string",
+        }),
+        defineField({
+            name:"heading",
+            title:"Heading",
+            type:"headingComponent",
+        })
+    ]
+})
